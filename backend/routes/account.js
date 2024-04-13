e5e37f8dc8df04c7caed9b744b32238dc6950647
chakripavankumar <siriyalapavankumar@gmail.com>
2024-04-13 16:46:50 +0530

    coded with  bad transfer route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -13,6 +13,43 @@ const Account= require("../db")
     })
   })
 
+  router.post("/transfer" ,  authMiddleware , async (req,res)=>{
+    const {amaount,to}=req.body;
+    const account = await Account .findone({
+      userId:req.userId
+    });
+    if(account.balance<amaount){
+      return res.status(400).json({
+        message:"insuficient balance"
+      })
+    }
+    const toAccount= await Account.findone({
+      userId:to
+    });
+    if(!toAccount){
+      res.status(400).json({
+        message: "Invalid account"
+      })
+    }
+     await Account.updateOne({
+      userId:req.userId
+     },{
+      $inc:{
+        balance:-amaount
+      }
+     })
+      await Account.updateOne({
+        userId:to
+      },{
+        $inc:{
+          balance:amaount
+        }
+      })
+      res.json({
+        message:"transfer successfull"
+      })
+  })
+
 
   module.exports={
     router
