fa98091e053df7be52d6b0c901f0a8e5b7849426
chakri pavan kumar <siriyalapavankumar@gmail.com>
2024-04-13 09:46:37 +0530

    account schema

--- backend/db.js
+++ backend/db.js
@@ -31,8 +31,21 @@
           }
  })
 
- const userModel = mongoose.model( "User" , userScheme);
+ const accountScheme= new mongoose.Schema({
+  userId: {
+     type:mongoose.Schema.Types.ObjectId,
+     ref:'User'
+  },
+  balance:{
+    type:Number,
+    required:true
+  }
+ })
+
+ const User = mongoose.model( "User" , userScheme);
+ const Account = mongoose.model("Account" , accountScheme)
 
  module.exports={
-      User
+      User,
+      Account
  }
