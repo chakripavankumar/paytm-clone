c0ab4e75cef1006f058e6b6f47f4d3cc23b500aa
chakri pavan kumar <siriyalapavankumar@gmail.com>
2024-04-13 15:26:43 +0530

    added account roues

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,13 @@
 const express = require("express");
 const cors= require("cors")
+const mainRouter=require("./routes/index");
 
+const app=express();
 
 app.use(cors());
 app.use(express.json());
 
-const mainRouter=require("./routes/index");
 
-const app=express();
 
 
 
--- backend/routes/index.js
+++ backend/routes/index.js
@@ -1,10 +1,9 @@
 const express= require("express");
-
-
-
 const UserRouter= require('./user');
+const accountRouter=require("./account")
 const router= express.Router();
 
-router.use("/user" ,UserRouter)
+router.use("/user" ,UserRouter);
+router.use("/account" , accountRouter)
  
-module.exports=router
+module.exports=router;
