7da078dc513f9edb457e66c48054e2e9975681d2
chakripavankumar <siriyalapavankumar@gmail.com>
2024-04-13 16:33:07 +0530

    added balance route

--- backend/routes/account.js
+++ backend/routes/account.js
@@ -1,7 +1,17 @@
 const express= require("express");
-
+const   authMiddleware= require("../middleware")
+const Account= require("../db")
 
   const router=  express.Router();
+
+  router.get("/balance" , authMiddleware, async(req,res)=>{
+    const account = await Account.findone({
+      userId:req.userId
+    });
+    res.json({
+      balance:account.balance
+    })
+  })
 
 
   module.exports={
