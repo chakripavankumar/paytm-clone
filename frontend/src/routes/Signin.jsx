676161ff0288c0f529fa5b65e0ef1fa335c00095
chakripavankumar <siriyalapavankumar@gmail.com>
2024-04-17 16:34:47 +0530

    signin & signup✅

--- frontend/src/routes/Signin.jsx
+++ frontend/src/routes/Signin.jsx
@@ -1,14 +1,26 @@
-import { Heading } from "../components/Heading"
-import { SubHeading } from "../components/SubHeading"
+import { Heading } from "../components/Heading";
+import { InputBox } from "../components/InputBox";
+import { SubHeading } from "../components/SubHeading";
+import{Button}  from "../components/Button";
+import {BottomWarning} from "../components/BottomWarning"
 
-
-const Signin = () => {
-  return (
-    <div>
-      <Heading label={"Signin"}/>
-      <SubHeading label={"Enter the required details to login into your account"}/>
+ function Signin(){
+  return(
+    <div className="bg-slate-100 h-screen flex justify-center">
+    <div className="flex flex-col  justify-center">
+    <div className="rounded-lg bg-white w-90 text-center p-2 h-max px-4">
+      <Heading label={"SignIn"}/>
+      <SubHeading label={"Enter your credentials to access your account"}/>
+      <InputBox label={"Email"} placeholder={"Enter yout email"}/>
+      <InputBox label={"Password"} placeholder={"Enter the password"}/>
+      <div className="pt-4">
+      <Button label={"SignIn"}/>
+      </div>
+      <BottomWarning label={"Don't have an account"} buttonText={"SignUp"} to={"/signup"}/>
+   </div>
+    </div>
     </div>
   )
 }
 
-export default Signin
+export default Signin;
