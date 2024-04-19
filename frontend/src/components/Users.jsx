434cf5ef7a647a002dfd7c8130106927d458e712
chakripavankumar <siriyalapavankumar@gmail.com>
2024-04-19 16:34:39 +0530

    dashboard✅

--- frontend/src/components/Users.jsx
+++ frontend/src/components/Users.jsx
@@ -1,7 +1,55 @@
-export function Users(){
+/* eslint-disable react/prop-types */
+import { useEffect, useState } from "react";
+import { Button } from "./Button";
+import axios from "axios";
+
+export const Users = () => {
+    // Replace with backend call
+    const [users,setUsers]= useState([]);
+    const [filter,setFilter]= useState("");
+     useEffect(()=>{
+       axios.get("http://localhost:3000/api/v1/user/bulk?filter" + filter)
+       .then(responce=> {
+        setUsers( responce.data.user)
+    })
+     },[])
+
     return (
-        <div>
-        
+        <>
+            <div className="font-bold mt-6 text-lg">
+                Users
+            </div>
+            <div className="my-2">
+                <input onChange={(e)=>{
+           setFilter(e.target.value)
+                }} type="text" placeholder="Search users..." className="w-full px-2 py-1 border rounded border-slate-200" />
+            </div>
+            <div>
+                {users.map(user => <User key={user._id} user={user} />)}
+            </div>
+        </>
+    );
+};
+
+function User({ user }) {
+    return (
+        <div className="flex justify-between">
+            <div className="flex">
+                <div className="rounded-full h-12 w-12 bg-slate-200 flex justify-center mt-1 mr-2">
+                    <div className="flex flex-col justify-center h-full text-xl">
+                        {user.firstName[0]}
+                    </div>
+                </div>
+                <div className="flex flex-col justify-center h-full">
+                    <div>
+                        {user.firstName} {user.lastName}
+                    </div>
+                </div>
+            </div>
+
+            <div className="flex flex-col justify-center h-full">
+                <Button label={"Send Money"} />
+            </div>
         </div>
-    )
+    );
 }
--- frontend/src/routes/Dashboard.jsx
+++ frontend/src/routes/Dashboard.jsx
@@ -1,11 +1,13 @@
 import { AppBar } from "../components/AppBar";
 import { Balance } from "../components/Balance";
+import { Users } from "../components/Users";
 
 function Dashboard(){
   return(
     <div>
    <AppBar/>
    <Balance value={"10,000"}/>
+   <Users/>
     </div>
   )
 }
